d6b7854970d2934e5ed66f66cfcadda055a1278f
Dean Joseph <captainwatch98@gmail.com>
2016-08-25 15:11:41 -0600

    half working blog pages

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,11 +3,42 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = @current_user.blog
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      flash[:success] = "Your Blog has been created"
+      redirect_to blogs_path
+    else
+      flash[:error] = "Error, please try again"
+      render :new
+    end
   end
 
   def edit
+    @blog = @current_user.blog
+  end
+
+  def update
+    @blog = @current_user.blog
+    if @blog.update
+      flash[:success] = "Your Blog has been edited"
+      redirect_to blog_path(@blog)
+    else
+      flash[:error] = "Error, please try again"
+      render :edit
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :body, :author)
   end
 end
