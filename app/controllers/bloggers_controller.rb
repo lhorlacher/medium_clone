3dbb0b020bdb7edfbb246efe4c45f9c40f02ad8f
Dean Joseph <captainwatch98@gmail.com>
2016-08-26 14:26:07 -0600

    bloggers show page with pagination

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -2,5 +2,6 @@ class BloggersController < ApplicationController
   def show
     @blogger = User.find(params[:id])
     @blogs = @blogger.blogs
+    @blogs = Blog.all.order('created_at').paginate(page: params[:page], per_page: 2)
   end
 end
