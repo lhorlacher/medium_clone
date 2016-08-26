522128615934ef09c57f7ba1948382eb93f58eee
Dean Joseph <captainwatch98@gmail.com>
2016-08-26 11:48:56 -0600

    commontator

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,7 @@ class User < ApplicationRecord
   has_attached_file :image, styles: { thumb: ["64x64#", :jpg] }
 
   validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-  
+
+  acts_as_commontator
+
 end
